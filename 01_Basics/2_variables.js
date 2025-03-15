a9dfb72acbac76091a105d28c8ce3f7300815e9a
Deepanshu Bansal <134117936+LogiCc96@users.noreply.github.com>
2025-03-15 11:44:18 +0000

    Variables and datatypes

---
+++ 01_Basics/2_variables.js
@@ -0,0 +1,12 @@
+
+// declaring const variable
+
+//updating the value of const is not allowed
+const accountId = 11223; 
+let accountEmail = "Deep@gmail.com";
+var accountPass = "Deppe390";
+accountCity = "Mumbai";
+
+console.table([accountId,accountEmail,accountPass,accountCity]);
+
+// Don't use var because of the scope problem. It doesn't have block scope.
---
+++ 01_Basics/3_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(Hello) // It has different syntax while using node so it will show error if we run here.
+
+let Name = "Deep"; 
+let age = 14;  
+let isloggedin = false; 
+
+console.log(typeof(Name));
+console.log(typeof(age));
+console.log(typeof(isloggedin));
+
+/*
+boolean true/false
+number -> 2 to power 53
+string
+bigint
+null -> standalone value, different from undefined
+undefined
+symbol
+
+ */
