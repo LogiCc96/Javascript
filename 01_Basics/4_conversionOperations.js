7f21d3ecc29fa7ad8ef21539c8bf010ed7b5725b
Deepanshu Bansal <134117936+LogiCc96@users.noreply.github.com>
2025-03-16 04:50:37 +0000

    Conversion

---
+++ 01_Basics/4_conversionOperations.js
@@ -0,0 +1,20 @@
+/*
+"33" => 33
+"33abc" => NaN(Not a Number)
+true => 1, false => 0;
+
+*/
+
+let isloggedIn = 1;
+let booleanisloggedIn = Boolean(isloggedIn);
+console.log(booleanisloggedIn);
+
+// 1 => true; 0 => false
+// "" => False
+// "Deep" => true
+
+//Number to String Conversion
+
+let num = 34;
+let stringnum = String(num);
+console.log(stringnum, typeof stringnum);
