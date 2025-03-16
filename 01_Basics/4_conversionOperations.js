5194df7b689f67cc8073a01549ca215b5c07ca25
Deepanshu Bansal <134117936+LogiCc96@users.noreply.github.com>
2025-03-16 05:08:10 +0000

    Operations

--- 01_Basics/4_conversionOperations.js
+++ 01_Basics/4_conversionOperations.js
@@ -7,7 +7,7 @@ true => 1, false => 0;
 
 let isloggedIn = 1;
 let booleanisloggedIn = Boolean(isloggedIn);
-console.log(booleanisloggedIn);
+// console.log(booleanisloggedIn);
 
 // 1 => true; 0 => false
 // "" => False
@@ -17,4 +17,13 @@ console.log(booleanisloggedIn);
 
 let num = 34;
 let stringnum = String(num);
-console.log(stringnum, typeof stringnum);
+// console.log(stringnum, typeof stringnum);
+
+// *****************  Operations  ***************************
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + 1 + "2");
+// console.log("1" + 2 + 2);
+
+
+
